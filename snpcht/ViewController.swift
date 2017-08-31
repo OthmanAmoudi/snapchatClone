2e9eafa8150620371a3f295ea8d7d348034fcc34
Othman Mashaab <red.fantastic@hotmail.com>
2017-08-31 18:02:08 +0300

    First Commit

---
+++ snpcht/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  snpcht
+//
+//  Created by Othman Mashaab on 31/08/2017.
+//  Copyright © 2017 Othman Mashaab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: AAPLCameraViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
