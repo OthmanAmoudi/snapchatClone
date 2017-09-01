60589c765257de4ea9a8139313a27baf5fb56602
Othman Mashaab <red.fantastic@hotmail.com>
2017-09-01 21:43:56 +0300

    Added the camera functuoonality

---
+++ snpcht/AAPLCameraVCDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AAPLCameraVCDelegate.h
+//  
+//
+//  Created by Othman Mashaab on 01/09/2017.
+//
+//
+
+#ifndef AAPLCameraVCDelegate_h
+#define AAPLCameraVCDelegate_h
+
+@protocol APPLCameraVCDelegate <NSObject>
+
+-(void)shouldEnableControls:(BOOL)enable;
+
+@end
+#endif /* AAPLCameraVCDelegate_h */
